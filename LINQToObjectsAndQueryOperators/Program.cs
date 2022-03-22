82d83461997c23c2897bc85f236b12252d0a7de5
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-22 22:52:33 +0100

    University manager created

--- LINQToObjectsAndQueryOperators/Program.cs
+++ LINQToObjectsAndQueryOperators/Program.cs
@@ -10,6 +10,85 @@ namespace LINQToObjectsAndQueryOperators
     {
         static void Main(string[] args)
         {
+            UniversityManager um = new UniversityManager();
+
+            um.MaleStudents();
+            um.FemaleStudents();
+
+            Console.ReadKey();
+        }
+    }
+
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        // constructor
+        public UniversityManager()
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+
+            // Let's add some Universities
+            universities.Add(new University() { Id = 1, Name = "Yale" });
+            universities.Add(new University() { Id = 2, Name = "Bejing Tech" });
+
+            // Let's add some Students
+            students.Add(new Student() { Id = 1, Name = "Carla", Gender = "female", Age = 17, UniversityId =  1});
+            students.Add(new Student() { Id = 2, Name = "Toni", Gender = "male", Age = 21, UniversityId =  1});
+            students.Add(new Student() { Id = 2, Name = "Frank", Gender = "male", Age = 20, UniversityId = 1 });
+            students.Add(new Student() { Id = 3, Name = "Leyla", Gender = "female", Age = 19, UniversityId =  2});
+            students.Add(new Student() { Id = 4, Name = "James", Gender = "trans-gender", Age = 25, UniversityId =  2});
+            students.Add(new Student() { Id = 5, Name = "Linda", Gender = "female", Age = 22, UniversityId =  2});
+        }
+
+        public void MaleStudents()
+        {
+            IEnumerable<Student> maleStudents = from student in students where student.Gender == "male" select student;
+            Console.WriteLine("Male - Students: ");
+
+            foreach(Student student in maleStudents)
+            {
+                student.Print();
+            }
+        }
+        public void FemaleStudents()
+        {
+            IEnumerable<Student> femaleStudents = from student in students where student.Gender == "female" select student;
+
+            Console.WriteLine("Female - Students: ");
+
+            foreach(Student student in femaleStudents)
+            {
+                student.Print();
+            }
+        }
+    }
+
+    class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("University {0} with id {1}", Name, Id);
+        }
+    }
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign Key
+        public int UniversityId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Student {0}, with Id {1}, Gender {2} and Age {3} from University with Id {4}", Name, Id, Gender, Age, UniversityId);
         }
     }
 }
